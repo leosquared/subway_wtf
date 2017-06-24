e43921c9011e824d0604c1a796ef4e636d27222c
leosquared <leo14358@gmail.com>
2017-06-24 17:30:28 -0400

    return 10 most recent twts

---
+++ 10tweets.py
@@ -0,0 +1,36 @@
+import requests, oauth2, json, csv, time
+from urllib.parse import urlencode
+from pprint import pprint
+
+with open('TWITTERCREDS.json', 'r') as credsfile:
+	creds = json.loads(credsfile.read())
+
+def api_client(consumer_key, consumer_secret, access_token, access_token_secret):
+    consumer = oauth2.Consumer(key=consumer_key, secret=consumer_secret)
+    token = oauth2.Token(key=access_token, secret=access_token_secret)
+    client = oauth2.Client(consumer, token)
+    return client
+
+
+def get_tweets():
+    search_endpoint = 'https://api.twitter.com/1.1/search/tweets.json'
+    search_term = urlencode({
+        'q':'from:NYCTSubway -filter:retweets -filter:replies'
+        , 'count':10
+    })
+    url = f'{search_endpoint}?{search_term}'
+    client = api_client(
+            creds.get('consumerKey')
+            , creds.get('consumerSecret')
+            , creds.get('accessToken')
+            , creds.get('accessTokenSecret')
+        )
+    r = client.request(url, method='GET', body=b'', headers=None)
+    statuses = json.loads(r[1]).get('statuses')
+    fmt_statuses = [{'tweet_id':x.get('id'), 'text':x.get('text'), 'created_time':x.get('created_at')} for x in statuses]
+    response = {
+        'statusCode': 200
+        , 'body': json.dumps(fmt_statuses)
+    }
+
+    return response
