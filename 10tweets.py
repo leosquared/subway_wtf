8e1152a1a7ff36857440dc2f1199dd3ffb8c55f1
shinson <sonia.hinson@gmail.com>
2017-06-27 01:08:27 -0400

    adding serverless to project

--- 10tweets.py
+++ 10tweets.py
@@ -1,18 +1,18 @@
+import unzip_requirements
 import requests, oauth2, json, csv, time
 from urllib.parse import urlencode
 from pprint import pprint
 
-with open('TWITTERCREDS.json', 'r') as credsfile:
-	creds = json.loads(credsfile.read())
+def get_tweets(event, context):
+    with open('TWITTERCREDS.json', 'r') as credsfile:
+        creds = json.loads(credsfile.read())
 
-def api_client(consumer_key, consumer_secret, access_token, access_token_secret):
-    consumer = oauth2.Consumer(key=consumer_key, secret=consumer_secret)
-    token = oauth2.Token(key=access_token, secret=access_token_secret)
-    client = oauth2.Client(consumer, token)
-    return client
+    def api_client(consumer_key, consumer_secret, access_token, access_token_secret):
+        consumer = oauth2.Consumer(key=consumer_key, secret=consumer_secret)
+        token = oauth2.Token(key=access_token, secret=access_token_secret)
+        client = oauth2.Client(consumer, token)
+        return client
 
-
-def get_tweets():
     search_endpoint = 'https://api.twitter.com/1.1/search/tweets.json'
     search_term = urlencode({
         'q':'from:NYCTSubway -filter:retweets -filter:replies'
@@ -28,6 +28,7 @@ def get_tweets():
     r = client.request(url, method='GET', body=b'', headers=None)
     statuses = json.loads(r[1]).get('statuses')
     fmt_statuses = [{'tweet_id':x.get('id'), 'text':x.get('text'), 'created_time':x.get('created_at')} for x in statuses]
+
     response = {
         'statusCode': 200
         , 'body': json.dumps(fmt_statuses)
